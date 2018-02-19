d1545650686111cc945bc0f29219218d2ed25a2d
Marcin Saj <31963121+marcinsaj@users.noreply.github.com>
2018-02-19 19:18:35 +0100

    Update Raspberry-Nixie-Shield-Nixie-Clock.py

--- code/Raspberry-Nixie-Shield-Nixie-Clock.py
+++ code/Raspberry-Nixie-Shield-Nixie-Clock.py
@@ -1,9 +1,8 @@
 '''
 Raspberry Nixie Shield - Nixie Clock
-This example shows how to control a nixie tube with a Raspberry Pi using Nixie Shield
-An example of using a state array and for loop
-Nixie Shield uses five digital outputs to drive nixie tube 
-Pin 29 as on/off (EN) line, 31, 33, 35, 37 as an address (A, B, C, D) of nixie tube digit/cathode
+
+-------- Code expample coming soon --------
+
 This example code is in the public domain
 https://www.nixietester.com
 '''
